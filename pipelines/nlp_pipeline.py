f9d95cefbe2e3fd63de1e369ac9a436d5744d312
ahsan <muhammad.ahsan@gmail.com>
2024-05-14 21:29:48 +0200

    renamed pipeline

--- pipelines/nlp_pipeline.py
+++ pipelines/nlp_pipeline.py
@@ -1,11 +1,11 @@
 from kfp import dsl
 
-from components.miscellaneous.string_ops import concat_op, reverse_op
+from components.miscellaneous.lwp_components.string_ops import concat_op, reverse_op
 
 
 @dsl.pipeline(name='text-transformer',
               description='A pipeline to concatenate two strings + reverse it')
-def custom_pipeline() -> str:
+def nlp_pipeline() -> str:
     concat_task: str = concat_op(a="first", b="second")
     reverse_task = reverse_op(a=concat_task.output)  # noqa
     return reverse_task.output  # noqa

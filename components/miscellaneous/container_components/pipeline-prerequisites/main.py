4667f442b2974fd95121871bd588093a2d92dfaa
ahsan <muhammad.ahsan@gmail.com>
2024-05-14 21:23:49 +0200

    Fixed logging and moved files

--- components/miscellaneous/container_components/pipeline-prerequisites/main.py
+++ components/miscellaneous/container_components/pipeline-prerequisites/main.py
@@ -1,6 +1,13 @@
+import logging.config
 import yaml
 from src.utils import check_dependency
 import dask.distributed as dd
+
+
+logging.config.fileConfig('logging_config.ini')
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -13,11 +20,13 @@ def main():
 
     success: bool = True
     for dependency, result in zip(dependencies, results):
-        print(f"{dependency['name']}: {'OK' if result else 'FAILED'}")
+        logger.info(f"{dependency['name']}: {'OK' if result else 'FAILED'}")
         if not result:
             success = False
     if not success:
-        raise Exception("Something requirements are not met")
+        msg = "Pipeline prerequisites failed to satisfy all requirements."
+        logger.error(msg)
+        raise Exception(msg)
 
 
 if __name__ == "__main__":
